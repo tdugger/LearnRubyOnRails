fdce8876989bbd5c6981de28e8e5092171c13e3a
tdugger <tdugger@troys-macbook-pro.local>
2013-08-19 15:54:47 -0500

    Adding files to git

---
+++ weblog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weblog::Application.config.secret_key_base = '0874563d02236972df78791c883d5471b0c902fe546690be17f3190164ded50b9cebbcba7fae12e5f15bebf2baad34446e826896728f4d0d93dd2b8b865fdbd6'
---
+++ weblog/doc/js/search_index.js
@@ -0,0 +1,1 @@
+var search_data = {"index":{"searchIndex":["readme"],"longSearchIndex":[""],"info":[["README","","README_rdoc.html","","<p>README\n<p>This README would normally document whatever steps are necessary to get the\napplication up and …\n"]]}}
